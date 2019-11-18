68c6777a9a38f364ca9e10a881cbf2bb2ac00932
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2019-11-18 11:08:34 -0500

    Added searching capabalities

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,22 @@
-// import React from "react"
-// import { Link, useStaticQuery, graphql } from "gatsby"
-// import Layout from "../components/layout";
-// export default () => {
-//     return (
-//         <Layout>
-//             Home Page
-//         </Layout>
-//     )
-// }
-import React, {useState} from "react"
+
+import React, { useState, useEffect } from "react"
 import Layout from "../components/layout"
 import { useStaticQuery, graphql } from "gatsby"
-import { ListItemText, List, ListItem, ListSubheader, ListItemSecondaryAction, Button } from "@material-ui/core"
+import { ListItemText, List, ListItem, ListSubheader, ListItemSecondaryAction, Button, TextField } from "@material-ui/core"
 import { Link } from "gatsby-theme-material-ui"
 import TagDialog from '../components/tagDialog'
+import { Autocomplete } from '@material-ui/lab'
+
+let containsAll = (arr, target) => target.every(v => arr.includes(v));
 
 export default () => {
   const [tagSelected, setTags] = useState([])
   const [post, setPost] = useState({})
   const [open, setOpen] = useState(false)
+  const [filterText, setFilterText] = useState("")
+  const [filterTags, setFilterTags] = useState([])
+  const [posts, setPosts] = useState([]);
+
   const data = useStaticQuery(graphql`
       query {
           allPrismicPost {
@@ -39,9 +37,48 @@ export default () => {
 
       }
   `)
-  const edges = data.allPrismicPost.edges;
+
+  let edges = [...data.allPrismicPost.edges];
+
+  const allTags = []
+  edges.forEach(edge => {
+    const tags = edge.node.tags
+    allTags.push(...tags)
+  })
+
+  const blogtags = Array.from(new Set(allTags))
+
+  useEffect(() => {
+    let change = data.allPrismicPost.edges.filter(edge => edge.node.data.title.text.toLowerCase().includes(filterText));
+    change = change.filter(edge => containsAll(edge.node.tags, filterTags))
+    setPosts(change)
+  }, [filterText, filterTags])
+  
   return (
     <Layout>
+      <br></br>
+      <TextField fullWidth
+        label="Filter Posts by Title"
+        variant="outlined"
+        value={filterText} onChange={e => setFilterText(e.target.value)}
+
+      />
+      <br />
+      <Autocomplete
+        multiple
+        id="tags-filled"
+        options={blogtags}
+        getOptionLabel={option => option}
+        defaultValue={[]}
+        onChange={(event, newValues) => setFilterTags(newValues)}
+        filterSelectedOptions
+        renderInput={params => (
+          <TextField
+            {...params} variant="outlined" label="Select Tags" placeholder="Tag" margin="normal"
+            fullWidth
+          />
+        )}
+      />
       <List
         subheader={
           <ListSubheader component="div">
@@ -49,12 +86,12 @@ export default () => {
           </ListSubheader>
         }
       >
-        {edges.map((edge, i) => (
+        {posts.map((edge, i) => (
 
           <ListItem button key={i}>
             <Link to={`/blog/${edge.node.uid}`}>
               <ListItemText primary={edge.node.data.title.text}
-                            secondary={`By Sharan Sajiv Menon - ${edge.node.data.published}`}/>
+                secondary={`By Sharan Sajiv Menon - ${edge.node.data.published}`} />
             </Link>
             <ListItemSecondaryAction>
               <Button onClick={() => {
@@ -62,7 +99,7 @@ export default () => {
                 setOpen(true)
                 setPost({
                   title: edge.node.data.title.text,
-                  author:'Sharan Sajiv Menon',
+                  author: 'Sharan Sajiv Menon',
                   published: edge.node.data.published
                 })
                 console.log("Hello")
@@ -71,7 +108,7 @@ export default () => {
           </ListItem>
         ))}
       </List>
-      <TagDialog open={open} tags={tagSelected} closeDialog={() => setOpen(false)} post={post}/>
+      <TagDialog open={open} tags={tagSelected} closeDialog={() => setOpen(false)} post={post} />
     </Layout>
   )
 }
